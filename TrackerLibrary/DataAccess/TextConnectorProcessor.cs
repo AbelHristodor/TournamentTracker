03951a385b58771728bd8d2fca321a1355dd8499
AbelHristodor <habelor@gmail.com>
2020-02-24 15:50:18 +0100

    Finished Lesson 11

---
+++ TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.Models;
+
+namespace TrackerLibrary.DataAccess.TextHelpers
+{
+    public static class TextConnectorProcessor
+    {
+        /// <summary>
+        /// Returns the full path of the text file
+        /// </summary>
+        /// <param name="fileName">Name of the file</param>
+        /// <returns>Full path of the file</returns>
+        public static string FullFilePath(this string fileName)
+        {
+            return $"{ ConfigurationManager.AppSettings["filepath"] }\\{ fileName }";
+        }
+
+        /// <summary>
+        /// Loads the text file
+        /// </summary>
+        /// <param name="file">Name of the file</param>
+        /// <returns>A list with the contents of the file</returns>
+        public static List<string> LoadFile(this string file)
+        {
+            if(!File.Exists(file))
+            {
+                return new List<string>();
+            }
+            return File.ReadAllLines(file).ToList();
+        }
+
+
+        /// <summary>
+        /// Converts a list of lines to PrizeModels
+        /// </summary>
+        /// <param name="lines">List of lines</param>
+        /// <returns>List of PrizeModels</returns>
+        public static List<PrizeModel> ConvertToPrizeModels(this List<string> lines)
+        {
+            List<PrizeModel> output = new List<PrizeModel>();
+            
+            foreach(string line in lines)
+            {
+                string[] cols = line.Split(',');
+
+                PrizeModel p = new PrizeModel
+                {
+                    Id = int.Parse(cols[0]),
+                    PlaceNumber = int.Parse(cols[1]),
+                    PlaceName = cols[2],
+                    PrizeAmount = decimal.Parse(cols[3]),
+                    PrizePercentage = double.Parse(cols[4])
+                };
+
+                output.Add(p);
+            }
+            return output;
+        }
+
+        /// <summary>
+        /// Saves PrizeModels to a text file.
+        /// </summary>
+        /// <param name="models">List of PrizeModels to save</param>
+        /// <param name="fileName">Name of the file</param>
+        public static void SaveToPrizeFile(this List<PrizeModel> models, string fileName)
+        {
+            List<string> lines = new List<string>();
+
+            foreach (PrizeModel p in models)
+            {
+                lines.Add($"{ p.Id },{ p.PlaceNumber },{ p.PlaceName },{ p.PrizeAmount },{ p.PrizePercentage }");
+            }
+
+            File.WriteAllLines(fileName.FullFilePath(), lines);
+        }
+    }
+}
